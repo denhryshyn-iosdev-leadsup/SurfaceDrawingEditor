85f1b53a282238040ad150efb9a26eb5b0b3c848
Denys Hryshyn <denis.hryshyn.0001@gmail.com>
2026-03-10 21:52:47 +0200

    back to prev canvas logic

--- Sources/SurfaceDrawingEditor/DrawingEditor/Public/DrawingEditorView.swift
+++ Sources/SurfaceDrawingEditor/DrawingEditor/Public/DrawingEditorView.swift
@@ -53,11 +53,7 @@ public struct DrawingEditorView: View {
     // MARK: - Canvas
 
     private var canvas: some View {
-        let aspect = image.size.width / image.size.height
-        let width = UIScreen.main.bounds.width
-        let height = width / aspect
-        
-        return ZoomableDrawingContainer(
+        ZoomableDrawingContainer(
             image: image,
             overlayImage: vm.autoOverlayImage,
             strokes: vm.strokes,
@@ -86,7 +82,6 @@ public struct DrawingEditorView: View {
             },
             zoomController: zoomController
         )
-        .frame(height: height)
         .clipped()
         .overlay {
             if vm.isProcessing || vm.isRenderingOverlay {
