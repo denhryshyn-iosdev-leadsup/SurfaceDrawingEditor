81ba24e2e105cc479187cdc21100f6a7946adbdb
Denys Hryshyn <denis.hryshyn.0001@gmail.com>
2026-03-31 23:50:37 +0300

    changed brush color opacity from 1.0 to 0.4

--- Sources/SurfaceDrawingEditor/DrawingEditor/Rendering/DrawingDesign.swift
+++ Sources/SurfaceDrawingEditor/DrawingEditor/Rendering/DrawingDesign.swift
@@ -15,6 +15,6 @@ enum DrawingDesign {
     static let brushColor = Color(hex: "#BEBEBE")
     static let eraserCursorColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.7)
     
-    static let brushDisplayColor = UIColor(red: 0xEB/255, green: 0x15/255, blue: 0x15/255, alpha: 1)
+    static let brushDisplayColor = UIColor(red: 0xEB/255, green: 0x15/255, blue: 0x15/255, alpha: 0.4)
     static let brushRenderColor = UIColor(red: 190/255, green: 190/255, blue: 190/255, alpha: 1)
 }
